5956e099c07fa0d12865274d9a1aecf830b6bb10
rohan <mailtorohan.97@gmail.com>
2025-06-29 00:19:42 +0530

    added

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,46 @@
+from utils.model_loader import Modelloader
+# from tools.weather_info_tool import WeatherInfoTool
+# from tools.place_search_tool import PlaceSearchTool
+# from tools.expense_calculator_tool import CalculatorTool
+# from tools.currency_conversion_tool import CurrencyConverterTool
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+
+class GraphBuilder():
+    def __init__(self):
+        self.tools = [
+            WeatherInfoTool(),
+            PlaceSearchTool(),
+            CalculatorTool(),
+            CurrencyConverterTool(),
+        ]
+        self.system_prompt = SYSTEM_PROMPT
+
+    def agent_function(self, state: MessagesState):
+        """ Main agent function"""
+        user_question = state["messages"]
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages": [response]}
+
+
+    def build_graph(self):
+        graph_builder = StateGraph(MessagesState)
+        graph_builder.add_node("agent",self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools = self.tools))
+        graph_builder.add_node(START, "agent")
+        graph_builder.add_conditional_edges("agent", tools_condition)
+        graph_builder.add_node("tools","agent")
+        graph_builder.add_node("agent", END)
+
+        self.graph = graph_builder.compile()
+        return self.graph
+
+
+    def __call__(self):
+        return self.build_graph()
+
+
